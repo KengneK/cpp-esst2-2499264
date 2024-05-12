7d127e08636eb53540d80c7fdba41f09cecb970b
KengneK <kkengne.kevin@yahoo.com>
2024-05-13 01:50:30 +0200

    challenge ch4 fertig!

--- ch4/challenge/groupingWords/main.cpp
+++ ch4/challenge/groupingWords/main.cpp
@@ -4,8 +4,14 @@
 #include <set>
 
 #include <algorithm> // for std::find
+#include <chrono>
 
-#define with_set 0 // o-> with std::map. 1 -> with std::set
+#define with_set 2 // o-> with std::map. 
+                   // 1 -> with std::set . 
+                   // 2-> Lösung des Kursdozent
+#define with_map_int_string 1 // Benutzen NUR in Block bei with_set == 0 
+                              // with_map_int_string = 1 -> benutzung von std::map< int, std::vector<std::string> >. 
+                              // with_map_int_string = 0 -> benutzung von std::map< std::string, std::vector<std::string> >
 
 #define NO_OF_CHARS 256
  
@@ -48,10 +54,20 @@ int main()
     std::vector<std::string> strs = {"eat","tea","tan","ate","nat","bat"};
     // Ergebnis soll ungefähr so aussehen: { {"bat"}, {"nat","tan"}, {"ate","eat","tea"} }
 
+    std::chrono::duration<double, std::milli> duration_time{};
+    auto t1 = std::chrono::high_resolution_clock::now();
+
 
 #if(with_set == 0)
+
+    #if(with_map_int_string == 1)
     std::map< int, std::vector<std::string> > anagrammGroups{};
     unsigned int id_of_group{0}; // wird als Key für anagrammGroups verwendet
+    #elif(with_map_int_string == 0)
+    std::map< std::string, std::vector<std::string> > anagrammGroups{};
+    std::string id_of_group{}; // wird als Key für anagrammGroups verwendet
+    #endif
+
     std::size_t strs_len {strs.size()};
     std::vector<std::size_t> index_worked{};// enthählt Index von String, die schon in anagrammGroups hinzugefügt wurden
 
@@ -60,9 +76,19 @@ int main()
             continue;
         }
         else{
+            #if(with_map_int_string == 1)
             anagrammGroups.insert({++id_of_group, std::vector<std::string>{strs.at(i)}});// Es wird eine neue Gruppe (vector von String) von Anagram in den map unter dem Key id_of_group hinzugefügt
                                                                                          // diese neue Grupe wird in der actuelle Schleifendurchgang mit allen vorhandenen Anagramen ausgefüllt. 
                                                                                          // das Key id_of_group bleibt dans unverändert bis wir eine andere anagram Gruppe finden
+            
+            #elif(with_map_int_string == 0)
+            //anagrammGroups.insert({id_of_group = strs.at(i), std::vector<std::string>{strs.at(i)}}) // ok
+            id_of_group = strs.at(i);
+            anagrammGroups.insert({id_of_group, std::vector<std::string>{strs.at(i)}});// Es wird eine neue Gruppe (vector von String) von Anagram in den map unter dem Key id_of_group hinzugefügt
+                                                                                         // diese neue Grupe wird in der actuelle Schleifendurchgang mit allen vorhandenen Anagramen ausgefüllt. 
+                                                                                         // das Key id_of_group bleibt dans unverändert bis wir eine andere anagram Gruppe finden
+                
+            #endif    
             //index_worked.push_back(i); // nicht notwendig hier. Aber in der for-Scheleife mit j schon,
         }
 
@@ -124,7 +150,43 @@ int main()
         }
         std::cout << std::endl;
     }
- #endif   
+
+#elif(with_set == 2)
+    std::map< std::string, std::vector<std::string> > groups;
+
+    auto wordToKey = [](const std::string &word) -> std::string {
+        std::set<char> letterSet; //le set va garder les char dans L'ordre alphabetique. Ainsi tous les caractere d'un mot seront rangé dans l'ordre alphabetique et cela va faciliter la comparaison entre les mots
+
+        for( const auto &letter : word)
+            letterSet.insert(letter);
+
+        std::string result;
+        for( const auto &letter : letterSet)
+            result += letter;
+
+        return result;
+    };
+
+    for( const auto &str : strs ){
+        const auto key = wordToKey(str); // le mot contenu dans str est mi dans ordre (alphabetique). Ainsi "eat","tea",et "ate" deviendront tous "aet" et seront tous conservé sous la clé "aet" grace a la ligne qui suit
+        groups[key].push_back(str);      //  Si la cle n'existe pas encore dans le group, alors wlle serra créé.
+    }
+
+
+    for( const auto &group : groups ){
+        std::cout << "Group " << group.first << ":" << std::endl;
+        for( const auto &word : group.second ){
+            std::cout << word << " ";
+        }
+        std::cout << std::endl;
+    }
+
+#endif  
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    duration_time = t2 - t1;
+    std::cout << "Duration = " << duration_time.count()<< std::endl;
+
 
     
     
