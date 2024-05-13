1e0b32a0989a06db8278fb99e3cefc1ff1cadab3
KengneK <kkengne.kevin@yahoo.com>
2024-05-13 17:28:45 +0200

    challenge ch5 fertig! kleine Korrekturen müssen gemacht werden wegen Warning-Meldung

--- ch5/challenge/YourErrorClass/main.cpp
+++ ch5/challenge/YourErrorClass/main.cpp
@@ -3,6 +3,23 @@
 #include <vector>
 #include <exception>
 
+class MyExceptionVector : public std::exception{
+
+public:
+    MyExceptionVector(char* text, std::size_t size_v, int bad_index) : m_text{text}, m_size{size_v}, m_index{bad_index} {}
+    virtual const char* what() const throw(){
+        std::string message {m_text};
+        message += "\nSize = " + std::to_string(m_size) + " and out of range index = " + std::to_string(m_index);
+        return message.data();
+    }
+
+private:
+    char* m_text;
+    std::size_t m_size;
+    int m_index;
+
+};
+
 int main()
 {
     std::vector<std::string> data;
@@ -18,7 +35,11 @@ int main()
         	// Dieses what() soll dann im Block zu catch(const std::exception &e) aufgerufen werden.
         	// Erweitere auch die Fehlermeldung so, dass die Größe des Vectors und welcher Wert vom Entwickler:in kam, 
         	// mit in der Exception auftauchen.
-            throw "Sorry, your array is too short.";
+
+            char* text = "Sorry, your array is too short.";
+            throw MyExceptionVector{text, data.size(), 10};
+            
+            //hrow MyExceptionVector{"Sorry, your array is too short.", data.size(), 10};
         }
 
         std::cout << data.at(10) << std::endl;
